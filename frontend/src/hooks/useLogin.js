e19e444eeb029dac1969ce406a2a4af681e35c9a
BTRON32 <131682089+BTRON32@users.noreply.github.com>
2025-06-07 22:20:50 +0530

    Update useLogin.js

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -1,11 +1,12 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login } from "../lib/api";
+import {toast} from "react-hot-toast"
 
 const useLogin = () => {
   const queryClient = useQueryClient();
   const { mutate, isPending, error } = useMutation({
     mutationFn: login,
-    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"] }, toast.success("User logged in successfully!")),
   });
 
   return { error, isPending, loginMutation: mutate };
